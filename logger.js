8dc2c58a1d2650dd06c2fd848a010ba14484f75b
fuck-bot01 <118105175+fuck-bot01@users.noreply.github.com>
2022-11-13 16:37:02 +0600

    Update logger.js

--- logger.js
+++ logger.js
@@ -2,15 +2,15 @@ const chalk = require('chalk');
 //const notifier = require('node-notifier');
 
 module.exports = (str, end) => {
-	console.log(chalk.hex('#9900FF').bold(`${end || '[ FCA-HZI ]'} > `) + str);
+	console.log(chalk.hex('#9900FF').bold(`${end || '[FJ-RIYAD]'} > `) + str);
 };
 module.exports.onLogger = (str,end,ctscolor) => { 
 	var checkbutdak = ctscolor.replace("#",'');
 	if (ctscolor.indexOf('#') != 1) {
-		console.log(chalk.hex('#00CCCC').bold(`${end || '[ FCA-HZI ]'} > `) + str);
+		console.log(chalk.hex('#00CCCC').bold(`${end || '[FJ-RIYAD]'} > `) + str);
 	}
 	else if (!isNaN(checkbutdak)) {
-		console.log(chalk.hex(ctscolor).bold(`${end || '[ FCA-HZI ]'} > `) + str);
+		console.log(chalk.hex(ctscolor).bold(`${end || '[FJ-RIYAD]'} > `) + str);
 	} 
-	else console.log(chalk.hex('#00CCCC').bold(`${end || '[ FCA-HZI ]'} > `) + str);
+	else console.log(chalk.hex('#00CCCC').bold(`${end || '[FJ-RIYAD]'} > `) + str);
 }
